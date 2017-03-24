0b3bcb7d3cf2e3c2711d96839d2b7ee92283159f
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-03-24 23:11:23 +0530

    Add Input Validation

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -11,15 +11,28 @@ module.exports = (req, res, next) => {
   req.sanitize('reg_no').trim();
   req.sanitize('password').trim();
 
-  //
-  // Some magic here.
+  req.getValidationResult().then((result) => {
+    if (!result.isEmpty()) {
+      let message = result.array().map((error) => error.msg).join('\n');
+      let err = new Error(message);
+      err.status = 403;
 
-  if (success) {
-    return next();
-  } else {
-    let err = new Error('Authentication failed.');
-    err.status = 403;
+      next(err);
 
-    return next(err);
-  }
+    } else {
+
+      //
+      // Some magic here.
+      let success = true;
+      if (success) {
+        return next();
+      } else {
+        let err = new Error('Authentication failed.');
+        err.status = 403;
+
+        next(err);
+      }
+    }
+  });
+
 }
