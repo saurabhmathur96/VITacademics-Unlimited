c4e89c9fb6328cbbc681d832291e54b97e7be6d7
Kushagra Vaish <vaish.kushagra@gmail.com>
2017-04-03 18:42:46 +0530

    Update deploy_ec2.sh

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh ubuntu@ec2-35-154-198-242.ap-south-1.compute.amazonaws.com <<'ENDSSH'
+ssh ubuntu@ec2-35-154-246-61.ap-south-1.compute.amazonaws.com <<'ENDSSH'
 cd VITAcademics-Unlimited/
 git pull
 npm install
