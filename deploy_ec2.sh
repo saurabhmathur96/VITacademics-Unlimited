25d825f6e555556633ae72aa812af3b5d9a56bb6
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-04-04 16:49:31 +0530

    Automate copying of faculty info

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -4,5 +4,6 @@ ssh ubuntu@ec2-35-154-246-61.ap-south-1.compute.amazonaws.com <<'ENDSSH'
 cd VITAcademics-Unlimited/
 git pull
 npm install
+cp ../FacultyInformationScraper/faculty_info.json data/
 pm2 restart api
 ENDSSH
