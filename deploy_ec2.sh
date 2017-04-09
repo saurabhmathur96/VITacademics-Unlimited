a7139f5f5dac1a39b4a9bb3aec57722cddf803bf
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-04-09 14:02:42 +0530

    Update autodeploy script

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -2,6 +2,7 @@
 
 ssh ubuntu@ec2-35-154-246-61.ap-south-1.compute.amazonaws.com <<'ENDSSH'
 cd VITAcademics-Unlimited/
+git stash
 git pull
 npm install
 cp ../FacultyInformationScraper/faculty_info.json data/
