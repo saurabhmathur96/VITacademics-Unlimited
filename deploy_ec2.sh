bd55b619452d747037bf28f023150565a6717a5d
Kushagra Vaish <vaish.kushagra@gmail.com>
2017-05-18 23:02:39 +0530

    Update deploy_ec2.sh

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ssh ubuntu@ec2-35-154-246-61.ap-south-1.compute.amazonaws.com <<'ENDSSH'
+ssh root@collegecode.org <<'ENDSSH'
 cd VITAcademics-Unlimited/
 git stash
 git pull
 npm install
-cp ../FacultyInformationScraper/faculty_info.json data/
+cp ../faculty_info.json data/
 pm2 restart api
 ENDSSH
