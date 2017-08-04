1c487cb396e5faf4aad4f410167643de48938960
Saurabh Mathur <saurabhmathur96@gmail.com>
2017-08-05 00:15:15 +0530

    Add new captcha parser for vtopbeta

--- src/utilities/CaptchaParserBeta.js
+++ src/utilities/CaptchaParserBeta.js
@@ -4,92 +4,26 @@ const ndarray = require('ndarray');
 const Promise = require('bluebird');
 const logger = require('winston');
 const path = require("path");
-const offsetMap = {
-  "A": 19,
-  "E": 20,
-  "4": 20,
-  "8": 22,
-  "3": 22,
-  "J": 21,
-  "V": 21,
-  "U": 22,
-  "1": 21,
-  "2": 21,
-  "I": 21,
-  "P": 19,
-  "F": 21,
-  "9": 23,
-  "6": 21,
-  "C": 21,
-  "N": 21,
-  "W": 22,
-  "7": 21,
-  "S": 19,
-  "5": 21,
-  "Z": 21,
-  "Q": 20,
-  "B": 21,
-  "R": 21,
-  "T": 21,
-  "H": 21,
-  "D": 21,
-  "Y": 21,
-  "X": 21,
-  "M": 21,
-  "K": 21,
-  "L": 21,
-  "G": 21
-}
+
 
 let filePath = path.join(__dirname, '..', '..', 'data', 'bitmaps.json');
 var bitmaps = JSON.parse(fs.readFileSync(filePath, "UTF-8"))
 
-function detectCharacter(image, shift, error) {
-  const characters = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
-  for (let i = 0; i < characters.length; i++) {
 
-    const character = characters[i];
-    const bitmap = bitmaps[character];
-
-    const offset = offsetMap[character] || 21
-
-    let total = 0
-    let matches = 0;
-    const length = bitmap.length;
-    const width = bitmap[0].length;
-    for (let y = 0; y < length; y++) { // iterate along length
-      for (let x = 0; x < width; x++) { // iterate along width
-        if ((bitmap[y][x] == 255) && (x + shift < 150) && (x + shift >= 0)) {
-          total += 1;
-          if (image.get(y + offset, x + shift) <= 10) {
-            matches += 1;
-          }
-        }
-      }
-    }
-
-    if (total != 0 && (matches / (total + 1e-6)) > error) {
-      return { 'character': character, 'width': width };
-    }
-
-  }
-  return null;
-
-}
 
 
 function makeBW(pixels) {
   // Grayscale, L = R * 299/1000 + G * 587/1000 + B * 114/1000
-  const height = pixels.shape[0]
-  const width = pixels.shape[1];
-  var bw = ndarray(new Uint8Array(height * width), [width, height]);
-  for (let y = 0; y < pixels.shape[1]; y++) {
-    for (let x = 0; x < pixels.shape[0]; x++) {
+  const height = pixels.shape[0],
+    width = pixels.shape[1];
+  var bw = ndarray(new Uint8Array(height * width), [height, width]);
+  for (let x = 0; x < pixels.shape[0]; x++) {
+    for (let y = 0; y < pixels.shape[1]; y++) {
       const pixel = pixels.get(x, y, 0) * 0.299 + // Red
         pixels.get(x, y, 1) * 0.587 + // Green
         pixels.get(x, y, 2) * 0.114; // Blue
       // Ignoring the alpha value at index 3 since all are 255.
-      bw.set(y, x, Math.ceil(pixel))
+      bw.set(x, y, pixel);
     }
   }
   return bw;
@@ -103,28 +37,46 @@ module.exports.getCaptcha = (url) => {
         logger.error(err);
         return reject(new Error('Error in parsing captcha.'));
       }
-      const bw = makeBW(pixels);
-      let characterRange = 0;
-      let width = 0;
+      pixels = makeBW(pixels);
+      for (let y = 1; y < 44; y++) {
+        for (let x = 1; x < 179; x++) {
+          const condition1 = (pixels.get(x, y - 1) === 255) && (pixels.get(x, y) === 0) && (pixels.get(x, y + 1) === 255);
+          const condition2 = (pixels.get(x - 1, y) === 255) && (pixels.get(x, y) == 0) && (pixels[x + 1, y] === 255);
+          const condition3 = (pixels.get(x, y) !== 255) && (pixels.get(x, y) !== 0);
+          if (condition1 || condition2 || condition3) {
+            pixels.set(x, y, 255);
+          }
+        }
+      }
       let captcha = "";
-     for (let iteration = 0; iteration < 6; iteration++) {
-        let result = null;
-        let error = 1;
-        while (result === null && error > 0.79) {
-            for (let x = 0; x < 20; x++) {
-                result = detectCharacter(bw, width + x, error);
-                if (result !== null) {
-                    characterRange = result.width;
-                    width += characterRange + x;
-                    captcha += result.character;
-                    break;
-                }
+      for (let s = 30; s < 181; s += 30) {
+        let matches = [];
+        const chars = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZ';
+        for (let c = 0; c < chars.length; c++) {
+          const current = chars[c];
+          let match = 0;
+          let black = 0;
+          const bitmap = bitmaps[current];
+          for (let x = 0; x < 32; x++) {
+            for (let y = 0; y < 30; y++) {
+              const x1 = y + s - 30
+              const y1 = x + 12
+              if ((pixels.get(x1, y1) === bitmap[x][y]) && (bitmap[x][y] == 0)) {
+                match += 1;
+              }
+              if (bitmap[x][y] == 0) {
+                black += 1;
+              }
             }
-            error -= 0.05;
+          }
+          if (match / black >= 0.80) {
+            const percent = match / black;
+            matches.push([percent, current]);
+          }
         }
-        if (result === null) {
-            break;
-        }
+        captcha += matches.reduce(function(a, b)  {
+          return a[0] > b[0] ? a : b
+        }, [0, 0])[1];
       }
       return resolve(captcha);
     })
