af7102799dd4a517df6f6f3abefdc726ea66bd33
Karishnu Poddar <karishnu@gmail.com>
2018-03-08 15:23:56 +0530

    gsid fix

--- src/scrapers/gsid.js
+++ src/scrapers/gsid.js
@@ -3,9 +3,11 @@ const Promise = require('bluebird');
 module.exports.getGsid = (html) => {
   return new Promise((resolve, reject) => {
     try {
-      const index = html.indexOf("gsid");
-      const id = html.substring(index, index + 12);
-      return resolve(id);
+      const index = html.match(/gsid=[0-9]{6,7};/);
+      const id = index[0];
+      const fid = id.substring(0, id.length-1);
+      console.log(fid);
+      return resolve(fid);
     } catch (ex) {
       reject(ex);
     }
